d93a6467cfa35355130c686f4b6c2a2906fa1570
Tobias Baumgarten <Tobias.b.Baumgarten@gmail.com>
2025-01-11 21:11:33 +0100

    add little doc to fn load_test_file and did a little change

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ pub mod day_01;
 pub mod day_02;
 pub mod day_03;
 
+/// used to load the test files easily
 fn load_test_file(day: u32) -> String {
     const BASE_PATH: &str = "src/test_files/";
     fs::read_to_string(format!("{BASE_PATH}day_{:02}.txt", day))
         .expect("Test file cannot be opened")
-        .to_string()
 }
