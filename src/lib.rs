1423356a700129626b660343fc10ebdc4b57f2c9
Tobias Baumgarten <Tobias.b.Baumgarten@gmail.com>
2025-02-08 11:31:39 +0100

    day 08 solved

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![doc = include_str!("../README.md")]
+
 use std::fs;
 
 pub mod day_01;
@@ -7,6 +9,7 @@ pub mod day_04;
 pub mod day_05;
 pub mod day_06;
 pub mod day_07;
+pub mod day_08;
 
 /// Loads the input by a given day
 /// # Example
