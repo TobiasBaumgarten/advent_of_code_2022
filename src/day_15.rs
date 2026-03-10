ed3ee6db2e782300252156b54ccb7fa77dc51fe8
Tobias Baumgarten <tobias.b.baumgarten@gmail.com>
2026-03-10 21:05:59 +0100

    solved day 15 star two

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,8 +1,9 @@
+use regex::Regex;
 use std::collections::HashSet;
 
-use regex::Regex;
+const FREQUENZY_FACTOR: i64 = 4000000;
 
-fn solve_puzzle_one(input: impl Into<String>, y: i32) -> i32 {
+pub fn solve_puzzle_one(input: impl Into<String>, y: i32) -> i32 {
     let sensors = parse_input(input);
     let max_x = sensors
         .iter()
@@ -14,7 +15,44 @@ fn solve_puzzle_one(input: impl Into<String>, y: i32) -> i32 {
         .map(|s| s.position.0 - s.distance() as i32)
         .min()
         .unwrap();
+    count_none_positions(&sensors, y, min_x, max_x)
+}
 
+pub fn solve_puzzle_two(input: impl Into<String>, max: i32) -> i64 {
+    let sensors = parse_input(input);
+
+    for sensor in &sensors {
+        // only seek the points around the sensor range
+        let r = sensor.distance() + 1; // increase range by 1
+        let x = sensor.position.0;
+        let y = sensor.position.1;
+
+        for point in get_manhatten_border(x, y, r) {
+            if !(point.0 >= 0 && point.0 <= max && point.1 >= 0 && point.1 <= max) {
+                continue;
+            }
+            if sensors.iter().all(|s| !s.in_range_point(&point)) {
+                return point.0 as i64 * FREQUENZY_FACTOR + point.1 as i64;
+            }
+        }
+    }
+
+    panic!("Ther isn't any position");
+}
+
+fn get_manhatten_border(x: i32, y: i32, r: i32) -> HashSet<Point> {
+    let mut border = HashSet::new();
+    for i in 0..=r {
+        // math rotation
+        border.insert(Point(x + (r - i), y - i));
+        border.insert(Point(x + i, y + (r - i)));
+        border.insert(Point(x - i, y - (r - i)));
+        border.insert(Point(x - (r - i), y + i));
+    }
+    border
+}
+
+fn count_none_positions(sensors: &Vec<Sensor>, y: i32, min_x: i32, max_x: i32) -> i32 {
     // check beacons on y
     let beacons_on_y: HashSet<i32> = sensors
         .iter()
@@ -26,7 +64,7 @@ fn solve_puzzle_one(input: impl Into<String>, y: i32) -> i32 {
     let mut count = -(beacons_on_y.len() as i32);
 
     for x in min_x..=max_x {
-        for sensor in &sensors {
+        for sensor in sensors {
             if sensor.in_range(x, y) {
                 count += 1;
                 break;
@@ -49,15 +87,20 @@ impl Sensor {
         Self { position, beacon }
     }
 
-    fn distance(&self) -> i64 {
-        (self.position.0 as i64 - self.beacon.0 as i64).abs()
-            + (self.position.1 as i64 - self.beacon.1 as i64).abs()
+    /// Returns the manhatten distance between the sensor positon and the beacon position.
+    fn distance(&self) -> i32 {
+        (self.position.0 - self.beacon.0).abs() + (self.position.1 - self.beacon.1).abs()
     }
 
-    fn in_range(&self, x: impl Into<i64>, y: impl Into<i64>) -> bool {
-        let squared =
-            (self.position.0 as i64 - x.into()).abs() + (self.position.1 as i64 - y.into()).abs();
+    /// Checks if the distance between the sensor and the beacon is
+    /// smaller then the distance between the sensor and the given point.
+    fn in_range(&self, x: impl Into<i32>, y: impl Into<i32>) -> bool {
+        let squared = (self.position.0 - x.into()).abs() + (self.position.1 - y.into()).abs();
         squared <= self.distance()
+    }
+
+    fn in_range_point(&self, point: &Point) -> bool {
+        self.in_range(point.0, point.1)
     }
 }
 
@@ -94,7 +137,7 @@ fn parse_input(input: impl Into<String>) -> Vec<Sensor> {
 #[cfg(test)]
 mod tests_day_15 {
     use crate::{
-        day_15::{parse_input, solve_puzzle_one},
+        day_15::{parse_input, solve_puzzle_one, solve_puzzle_two},
         load_input,
     };
 
@@ -126,10 +169,23 @@ mod tests_day_15 {
     }
 
     #[test]
+    fn star_two_example() {
+        let sum = solve_puzzle_two(EXAMPLE, 20);
+        assert_eq!(sum, 56000011);
+    }
+
+    #[test]
     fn star_one_input() {
         let input = load_input(15);
         let count = solve_puzzle_one(input, 2000000);
 
         assert_eq!(count, 6078701);
+    }
+
+    #[test]
+    fn star_two_input() {
+        let input = load_input(15);
+        let sum = solve_puzzle_two(input, 4000000);
+        assert_eq!(sum, 12567351400528);
     }
 }
